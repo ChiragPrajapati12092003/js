7e325e23ba62625b906e34159a1d1da4b8c199df
chirag_prajapati <124158107+ChiragPrajapati12092003@users.noreply.github.com>
2024-01-26 15:29:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version 
+
+// alert(3 + 3) we are using nodejs , not browser
+
+console.log(3
+    +
+    3); //code readability should be high 
+
+console.log("chirag");
+
+let name ="chirag"
+let age = 18
+let isLoggedIn = false
+
+
+//permeative {
+//number => 2 to power 53
+//bigint => bade nuber ke liye 
+// string =>""
+//boolean => true/false
+//null => standalone value(type) khali hai ek type hai means empty 
+//undefined =>abhi value assign nai hui hai
+//symbol => unique 
+//}
+
+
+console.log(typeof "chirag");
+console.log(typeof age);
+console.log(typeof null); //ek problem hai object ki tarah dikhta hai interview question
+console.log(typeof undefined);
