17d804fb8f5416a5ca529ca71064a2a12c606c18
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-06-21 10:41:29 +0530

    info about inheritance

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,33 @@
+class User{
+    constructor(username) {
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+} 
+
+const chai = new Teacher("chai", "chjcb", "655")
+chai.addCourse()
+chai.logMe()
+
+const masalaChai = new User("masalaChai")
+// masalaChai.addCourse() // iske pass access nai hai
+masalaChai.logMe()
+
+console.log(chai === masalaChai);// flase
+console.log(chai instanceof Teacher); // true
+console.log(chai instanceof User); // true
